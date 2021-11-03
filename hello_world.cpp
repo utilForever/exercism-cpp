67b6a1941a4ba6038e8ed01c51347da89074e397
Chris Ohk <utilForever@gmail.com>
2021-11-03 14:58:01 +0900

    Upload exercise "Hello World"

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include "hello_world.h"
+
+namespace hello_world
+{
+std::string hello()
+{
+    return "Hello, World!";
+}
+}
---
+++ hello_world.h
@@ -0,0 +1,11 @@
+#if !defined(HELLO_WORLD_H)
+#define HELLO_WORLD_H
+
+#include <string>
+
+namespace hello_world
+{
+std::string hello();
+}  // namespace hello_world
+
+#endif // HELLO_WORLD_H
